951d884d7ed81702eb2bc1f9a9f74387fce6e92b
weixiangchen520 <1981468862@qq.com>
2024-03-15 00:03:40 +0800

    feat: init examples

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import type { IApi } from 'umi';
+import { IApi } from 'umi';
 
 export default (api: IApi) => {
-  // See https://umijs.org/docs/guides/plugins
+  api.describe({
+    key: 'runtimeImport',
+    config: {
+      schema(joi) {
+        return joi.string();
+      },
+    },
+    enableBy: api.EnableBy.config
+  });
+  api.modifyConfig((memo)=>{
+    console.log('test runtimeImpoddrtd');
+    memo.favicons = api.userConfig.changeFavicon;
+    return memo;
+  });
 };
